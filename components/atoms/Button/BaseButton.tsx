33f7cbdb4002a74698ac03d968acd82295448f10
LuigiCleffi <luigicleffi674@gmail.com>
2026-01-29 10:08:27 -0300

    Refactor BaseButton component to improve variant handling and optimize rendering

--- components/atoms/Button/BaseButton.tsx
+++ components/atoms/Button/BaseButton.tsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 interface BaseButtonProps {
     variant?: 'primary' | 'dark';
     text: string;
 }
 
-export function BaseButton({ variant, text }: BaseButtonProps) {
-const handleVariant = () => {
+const handleVariant = (variant: BaseButtonProps["variant"]) => {
     const variants = {
         primary: 'w-24 rounded-full bg-white px-4 py-2 font-bold text-black md:w-32 lg:w-40',
         dark: 'w-24 rounded-full bg-gray-700 px-4 py-2 font-bold text-white md:w-32 lg:w-40',
     };
-    return variants[variant || 'primary'];
-}
+    return variants[variant ?? 'primary'];
+};
+
+export function BaseButton({ variant, text }: BaseButtonProps) {
+  const buttonClass = useMemo(() => handleVariant(variant), [variant]);
+
   return (
-    <button className={handleVariant()}>
+    <button className={buttonClass}>
       {text}
     </button>
   );
